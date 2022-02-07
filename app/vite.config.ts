57494436e1506c896f8101141d969241c3dbb9da
nkroe <nikitak123@mail.ru>
2022-02-07 11:39:49 +0500

    feature/focus-trap

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,18 @@
+import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [viteCommonjs(), react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        // Solves:
+        // https://github.com/vitejs/vite/issues/5308
+        // add the name of your package
+        esbuildCommonjs(['focus-trap-react']),
+      ],
+    },
+  },
 });
