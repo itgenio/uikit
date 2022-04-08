724eaa15836b20bab79f3f47b02ef4e52aabd5cf
Siarhey1607 <izaspenec@gmail.com>
2022-04-08 12:37:06 +0200

    checkbox

--- app/src/pages/uikit/index.tsx
+++ app/src/pages/uikit/index.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import { DarkModeButton } from '../../components/darkModeButton';
 import { ButtonGroups } from './components/buttonGroup';
 import { Buttons } from './components/buttons';
+import { Checkboxes } from './components/checkboxes';
 import { Colors } from './components/colors';
 import { Dialogs } from './components/dialogs';
 import { Links } from './components/links';
@@ -12,7 +13,18 @@ import { Panels } from './components/panels';
 import { TabsExample } from './components/tabs';
 import { TextFields } from './components/textFields';
 
-const components = [Colors, Buttons, ButtonGroups, Links, Panels, TabsExample, TextFields, Dialogs, ModalPages];
+const components = [
+  Colors,
+  Buttons,
+  ButtonGroups,
+  Links,
+  Panels,
+  TabsExample,
+  TextFields,
+  Dialogs,
+  ModalPages,
+  Checkboxes,
+];
 const DEFAULT = 0;
 
 export function UikitDemo() {
