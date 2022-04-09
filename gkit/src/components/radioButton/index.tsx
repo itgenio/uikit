cdee4d99167be18a3ab194e73cce5d550b901e81
Siarhey1607 <izaspenec@gmail.com>
2022-04-09 15:18:32 +0200

    fix onChange, margin, className

--- gkit/src/components/radioButton/index.tsx
+++ gkit/src/components/radioButton/index.tsx
@@ -6,7 +6,7 @@ export type RadioButtonProps = PropsWithChildren<{
   disabled?: boolean;
   hover?: boolean;
   checked?: boolean;
-  onChange?: (ev: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
   className?: string;
   idQa?: string;
 }>;
@@ -16,13 +16,13 @@ export function RadioButton({ children, hover, disabled, checked, idQa, onChange
     <label className={classNames('gkit-radio-button', className)}>
       <input
         type="radio"
-        className={classNames('radio-input', className, { hover })}
+        className={classNames('radio-input', { hover })}
         id-qa={idQa}
         disabled={disabled}
         checked={checked}
         onChange={onChange}
       />
-      <span>{children}</span>
+      <span className="radio-button-span">{children}</span>
     </label>
   );
 }
