786f23b76113eeb76e4ed633795ef685144ce831
Siarhey1607 <izaspenec@gmail.com>
2022-04-12 12:58:16 +0200

    мелкие правки

--- gkit/src/components/toggle/index.tsx
+++ gkit/src/components/toggle/index.tsx
@@ -6,24 +6,21 @@ export type ToggleProps = PropsWithChildren<{
   disabled?: boolean;
   hover?: boolean;
   checked?: boolean;
-  onChange?: (ev: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
   className?: string;
   idQa?: string;
 }>;
 
 export function Toggle({ children, hover, disabled, checked, idQa, onChange, className }: ToggleProps) {
   return (
-    <label className="gkit-toggle">
+    <label className={classNames('gkit-toggle', className)} id-qa={idQa}>
       <input
-        className={classNames('toggle-input', className, { hover, disabled, checked })}
+        className={classNames('toggle-input', { hover, disabled, checked })}
         type="checkbox"
-        disabled={disabled}
-        checked={checked}
-        id-qa={idQa}
-        onChange={onChange}
+        {...{ onChange, checked, disabled }}
       />
       <span className="slider round" />
-      <span className={classNames('toggle-text', { disabled, checked })}>{children}</span>
+      {children && <span className={classNames('toggle-text', { disabled, checked })}>{children}</span>}
     </label>
   );
 }
