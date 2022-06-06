b7a346d572747da4161766f946f8da289ae0839a
AndreevAndrey <888Andrei888@mail.ru>
2022-06-06 11:52:38 +0000

    added CalendarWithArrowRightIcon

--- app/src/pages/uikit/components/icons/index.tsx
+++ app/src/pages/uikit/components/icons/index.tsx
@@ -14,6 +14,7 @@ import {
   CalendarIcon,
   MoneyIcon,
   DocumentCopyIcon,
+  CalendarWithArrowRightIcon,
 } from '@itgenio/gkit';
 
 export function Icons() {
@@ -31,6 +32,7 @@ export function Icons() {
       <CalendarIcon />
       <MoneyIcon />
       <DocumentCopyIcon />
+      <CalendarWithArrowRightIcon />
     </div>
   );
 }
