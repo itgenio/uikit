31798088fa954b8e271bd5be5bbb5077261cb65c
Sergey Bulovetski <83006103+Siarhei1607@users.noreply.github.com>
2022-06-21 12:39:06 +0200

    search-icon (#41)

--- app/src/pages/uikit/components/icons/index.tsx
+++ app/src/pages/uikit/components/icons/index.tsx
@@ -19,6 +19,7 @@ import {
   HeartIcon,
   HeartFillIcon,
   OptionsIcon,
+  SearchIcon,
 } from '@itgenio/gkit';
 
 const ICONS = [
@@ -39,6 +40,7 @@ const ICONS = [
   HeartIcon,
   HeartFillIcon,
   OptionsIcon,
+  SearchIcon,
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export function Icons() {
