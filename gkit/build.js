9c48f0416a4bd5e48e4593811eb561e4e138fbb7
OlshaMB <michaelolshanskiy@icloud.com>
2022-06-25 13:10:59 +0300

    feat!{Не готов, Не работает, Нужно починить}: Добавлен turborepo в build систему, обновленны несколько параметров в build systemы.
    Добавлен turbo.json для turborepo.
    
    Удалён скрипт watch в gkit. Т.к. с будущей реализацией turborepo он будет только мешать.
    
    Добалена зависимость в app для livereload при turborepo. Коментарий: Эта зависимость взята с примера turborepo по vite https://github.com/vercel/turborepo/blob/main/examples/kitchen-sink/apps/admin/vite.config.ts.
    
    Автор: OlshaMB
    Refs: #47
    Материалы:
    - https://github.com/vercel/turborepo/blob/main/examples/kitchen-sink/apps/admin/vite.config.ts
    - https://turborepo.org/docs/getting-started
    - https://dev.to/nicolaserny/create-a-react-component-library-with-vite-and-typescript-1ih9

--- gkit/build.js
+++ gkit/build.js
@@ -2,20 +2,8 @@ const path = require('path');
 const { build } = require('esbuild');
 const { lessLoader } = require('esbuild-plugin-less');
 
-// isProduction flag for watch mode
-const isProduction = process.env.NODE_ENV === 'production';
-
 (async () => {
   build({
-    watch: isProduction
-      ? false
-      : {
-          onRebuild(error) {
-            if (!error) {
-              console.log('Build succeeded');
-            }
-          },
-        },
     entryPoints: [path.resolve(__dirname, 'src/index.ts')],
     outbase: 'src',
     bundle: true,
