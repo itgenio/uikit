22ab20a0f936270a6f11adfe3ea7a458b3183b89
nkroe <nikitak123@mail.ru>
2022-07-04 17:40:58 +0500

    small refactor

--- gkit/src/components/icons/add.tsx
+++ gkit/src/components/icons/add.tsx
@@ -1,11 +1,11 @@
 import classNames from 'classnames';
 import React from 'react';
 import AddFilled from './assets/add_24_filled.svg';
-import AddRegular from './assets/add_24_regular.svg';
+import Add from './assets/add_24_regular.svg';
 import { SvgIconProps } from './types';
 
 export function AddIcon({ className, ...props }: Partial<SvgIconProps> = {}) {
-  return <AddRegular className={classNames('add-icon', className)} {...props} />;
+  return <Add className={classNames('add-icon', className)} {...props} />;
 }
 
 export function AddFilledIcon({ className, ...props }: Partial<SvgIconProps> = {}) {
