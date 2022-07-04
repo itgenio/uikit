ed9975dfe5ee3c521fd50e1b6ee99654043037e7
Nikita <ntsyklinskiy@gmail.com>
2022-07-04 16:25:32 +0300

    fix

--- gkit/src/components/dropdown/index.tsx
+++ gkit/src/components/dropdown/index.tsx
@@ -64,7 +64,7 @@ export const Dropdown = React.memo(
       } else {
         setSelectedAll(false);
       }
-    }, [selectedValues, options.length]);
+    }, [selectedValues, options.length, onChange]);
 
     useLayoutEffect(() => {
       if (!open) return;
