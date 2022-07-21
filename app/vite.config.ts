487e9661dea64b9d21d9512ba118d720afc0772a
nkroe <nikitak123@mail.ru>
2022-07-21 22:31:14 +0500

    test app

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@itgenio/gkit'],
+      external: [/^@itgenio\/gkit\/\w+$/],
     },
   },
 });
