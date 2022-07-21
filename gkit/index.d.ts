f89a2e185e39b61914d97123a68b16ab21090f2f
nkroe <nikitak123@mail.ru>
2022-07-21 13:47:44 +0500

    test tsc

--- gkit/index.d.ts
+++ gkit/index.d.ts
@@ -1,3 +1,24 @@
+import './index.css';
+export * from './button';
+export * from './tabs';
+export * from './dialog';
+export * from './link';
+export * from './modalPage';
+export * from './textArea';
+export * from './textField';
+export * from './select';
+export * from './multiSelect';
+export * from './popover';
+export * from './switcher';
+export * from './checkbox';
+export * from './accordion';
+export * from './radioButton';
+export * from './toggle';
+export * from './tooltip';
+export * from './badge';
+export * from './icons';
+
+
 declare module '*.svg' {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
 
