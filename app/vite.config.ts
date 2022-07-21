f05f6af14d3d195d7762e74be7c9c3d4e684f81a
nkroe <nikitak123@mail.ru>
2022-07-21 17:57:08 +0500

    test app

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-// @ts-expect-error
-const path = require('path');
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // @ts-expect-error
-  resolve: { alias: [{ find: '@itgenio/gkit', replacement: path.resolve(__dirname, './node_modules/@itgenio/gkit') }] },
+  resolve: { preserveSymlinks: true },
 });
