507f6ab40452ab854c6587c97e626e100b6c90a0
Nikita Kroe <nikitak123@mail.ru>
2022-08-12 08:33:19 +0500

    add id-qa and disablePrevent to Form component

--- gkit/src/components/form/index.tsx
+++ gkit/src/components/form/index.tsx
@@ -1,14 +1,16 @@
 import classNames from 'classnames';
 import React, { PropsWithChildren } from 'react';
 
-type Props = PropsWithChildren<{ className?: string }>;
+type Props = PropsWithChildren<{ className?: string; disablePrevent?: boolean; idQa?: string }>;
 
-export const Form = React.memo(({ className, children }: Props) => {
+export const Form = ({ className, disablePrevent, idQa, children }: Props) => {
   return (
-    <form className={classNames('gkit-form', className)} onSubmit={e => e.preventDefault()}>
+    <form
+      className={classNames('gkit-form', className)}
+      id-qa={idQa}
+      onSubmit={e => !disablePrevent && e.preventDefault()}
+    >
       {children}
     </form>
   );
-});
-
-Form.displayName = 'Form';
+};
