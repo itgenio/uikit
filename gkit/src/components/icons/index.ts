5af483b86eba3d27eac4f1777bbbcfa7c8d98548
AndreyAndreev <888andrei1983888@gmail.com>
2022-08-16 11:48:51 +0000

    added like,dislike icons

--- gkit/src/components/icons/index.ts
+++ gkit/src/components/icons/index.ts
@@ -30,3 +30,5 @@ export * from './textBulletListTree';
 export * from './micSparkle';
 export * from './peopleCommunity';
 export * from './chatHelp';
+export * from './like';
+export * from './dislike';
