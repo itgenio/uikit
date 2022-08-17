61b62150d21755d15d5fe929933ecc95110719c1
afrokick <devbyru@gmail.com>
2022-08-18 01:52:54 +0300

    add select from radix

--- gkit/src/components/select/index.tsx
+++ gkit/src/components/select/index.tsx
@@ -1,4 +1,5 @@
 import './style.less';
+import * as SelectPrimitive from '@radix-ui/react-select';
 import classNames from 'classnames';
 import React, { useLayoutEffect, useMemo, useRef, useState } from 'react';
 import useOnClickOutside from 'use-onclickoutside';
@@ -9,7 +10,7 @@ import { generateId } from '../internal/utils/generateId';
 
 type Sizes = 'small' | 'large';
 
-type Values = string | number;
+type Values = string;
 
 export type SelectOption = { label: string; value: Values; group?: string };
 
@@ -55,44 +56,50 @@ export const Select = React.memo(
     const [open, setOpen] = useState(false);
     const id = useMemo(() => generateId(), []);
     const ref = useRef<HTMLDivElement>(null);
-    const dropdownRef = useRef<HTMLDivElement>(null);
+    // const dropdownRef = useRef<HTMLDivElement>(null);
 
     const canShowDropdown = open && !disabled;
 
     useOnClickOutside(ref, () => setOpen(false));
 
-    useLayoutEffect(() => {
-      if (!open) return;
-
-      const dropdownElement = dropdownRef.current;
-
-      if (!dropdownElement) return;
-
-      const rect = dropdownElement.getBoundingClientRect();
-
-      if (rect.right > window.innerWidth) {
-        dropdownElement.style.left = `-${rect.right - window.innerWidth + DROPDOWN_PADDING}px`;
-      }
-
-      if (rect.bottom > window.innerHeight) {
-        dropdownElement.style.top = `calc(100% - ${rect.bottom - window.innerHeight + DROPDOWN_PADDING}px)`;
-      }
-    }, [open]);
+    // useLayoutEffect(() => {
+    //   if (!open) return;
+    //
+    //   const dropdownElement = dropdownRef.current;
+    //
+    //   if (!dropdownElement) return;
+    //
+    //   const rect = dropdownElement.getBoundingClientRect();
+    //
+    //   if (rect.right > window.innerWidth) {
+    //     dropdownElement.style.left = `-${rect.right - window.innerWidth + DROPDOWN_PADDING}px`;
+    //   }
+    //
+    //   if (rect.bottom > window.innerHeight) {
+    //     dropdownElement.style.top = `calc(100% - ${rect.bottom - window.innerHeight + DROPDOWN_PADDING}px)`;
+    //   }
+    // }, [open]);
 
     const renderOptionItem = (option: SelectOption, index: number) => (
-      <div
+      <SelectPrimitive.Item
         id-qa={classNames({ [`${idQa}-option-${option.value}`]: idQa })}
-        className={classNames('select-option', size)}
+        className={classNames('gkit-select-option', size)}
         key={index}
         onClick={e => {
           e.stopPropagation();
           setOpen(!open);
           onChange(option.value);
         }}
+        value={option.value}
+        data-value={option.value}
       >
-        {option.label}
-        {option.value === value && <CheckmarkIcon />}
-      </div>
+        <SelectPrimitive.ItemText>{option.label}</SelectPrimitive.ItemText>
+        {option.value === value && (
+          <SelectPrimitive.ItemIndicator>
+            <CheckmarkIcon />
+          </SelectPrimitive.ItemIndicator>
+        )}
+      </SelectPrimitive.Item>
     );
 
     const renderOptionsByGroups = () => {
@@ -105,47 +112,110 @@ export const Select = React.memo(
         options.map(renderOptionItem),
 
         index !== groupedOptions.length - 1 && (
-          <div key={`divider-${index}`} tabIndex={-1}>
-            <hr />
-          </div>
+          <SelectPrimitive.Separator key={`divider-${index}`} className="gkit-select-separator" />
         ),
       ]);
     };
 
     return (
       <InputsContainer {...{ id, size, label, helperText, idQa, className }}>
-        <div
-          ref={ref}
-          className={classNames('gkit-select', size, {
-            hover,
-            focus: focus || canShowDropdown,
-            error,
-            disabled,
-          })}
-          onClick={() => setOpen(!open)}
-        >
-          <div className="input-wrapper">
-            <input
-              readOnly
-              className={classNames('select-input', size, { filled, error, disabled })}
-              {...{ id, placeholder, disabled }}
-              value={options.find(option => option.value === value)?.label}
-            />
-
-            <div className="select-chevron">
+        <SelectPrimitive.Root value={value} onValueChange={onChange} open={disabled ? false : undefined}>
+          <SelectPrimitive.Trigger
+            className={classNames('gkit-select', 'input-wrapper', size, {
+              hover,
+              focus: focus || canShowDropdown,
+              error,
+              disabled,
+            })}
+            id-qa={classNames({ [`${idQa}-trigger`]: idQa })}
+          >
+            <SelectPrimitive.Value placeholder={placeholder} aria-label={value} />
+            <SelectPrimitive.Icon className="select-chevron">
               {canShowDropdown ? <ChevronUpFilledIcon /> : <ChevronDownFilledIcon />}
-            </div>
-          </div>
+            </SelectPrimitive.Icon>
+          </SelectPrimitive.Trigger>
+          <SelectPrimitive.Portal>
+            <SelectPrimitive.Content
+              className="gkit-select-dropdown"
+              id-qa={classNames({ [`${idQa}-dropdown`]: idQa })}
+            >
+              <SelectPrimitive.Viewport>
+                {divideByGroups ? renderOptionsByGroups() : options.map(renderOptionItem)}
+              </SelectPrimitive.Viewport>
+            </SelectPrimitive.Content>
+          </SelectPrimitive.Portal>
+        </SelectPrimitive.Root>
+        {/*<div*/}
+        {/*  ref={ref}*/}
+        {/*  className={classNames('gkit-select', size, {*/}
+        {/*    hover,*/}
+        {/*    focus: focus || canShowDropdown,*/}
+        {/*    error,*/}
+        {/*    disabled,*/}
+        {/*  })}*/}
+        {/*  onClick={() => setOpen(!open)}*/}
+        {/*>*/}
+        {/*  <div className="input-wrapper">*/}
+        {/*    <input*/}
+        {/*      readOnly*/}
+        {/*      className={classNames('select-input', size, { filled, error, disabled })}*/}
+        {/*      {...{ id, placeholder, disabled }}*/}
+        {/*      value={options.find(option => option.value === value)?.label}*/}
+        {/*    />*/}
 
-          {canShowDropdown && (
-            <div className="select-dropdown" id-qa={classNames({ [`${idQa}-dropdown`]: idQa })} ref={dropdownRef}>
-              {divideByGroups ? renderOptionsByGroups() : options.map(renderOptionItem)}
-            </div>
-          )}
-        </div>
+        {/*    <div className="select-chevron">*/}
+        {/*      {canShowDropdown ? <ChevronUpFilledIcon /> : <ChevronDownFilledIcon />}*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+
+        {/*  {canShowDropdown && (*/}
+        {/*    <div className="select-dropdown" id-qa={classNames({ [`${idQa}-dropdown`]: idQa })} ref={dropdownRef}>*/}
+        {/*      {divideByGroups ? renderOptionsByGroups() : options.map(renderOptionItem)}*/}
+        {/*    </div>*/}
+        {/*  )}*/}
+        {/*</div>*/}
       </InputsContainer>
     );
   }
 );
 
 Select.displayName = 'Select';
+
+// export const Select = React.forwardRef(
+//   ({ children, ...props }, forwardedRef) => {
+//     return (
+//       <SelectPrimitive.Root {...props}>
+//         <SelectPrimitive.Trigger ref={forwardedRef}>
+//           <SelectPrimitive.Value />
+//           <SelectPrimitive.Icon>
+//             <ChevronDownIcon />
+//           </SelectPrimitive.Icon>
+//         </SelectPrimitive.Trigger>
+//         <SelectPrimitive.Portal>
+//           <SelectPrimitive.Content>
+//             <SelectPrimitive.ScrollUpButton>
+//               <ChevronUpIcon />
+//             </SelectPrimitive.ScrollUpButton>
+//             <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
+//             <SelectPrimitive.ScrollDownButton>
+//               <ChevronDownIcon />
+//             </SelectPrimitive.ScrollDownButton>
+//           </SelectPrimitive.Content>
+//         </SelectPrimitive.Portal>
+//       </SelectPrimitive.Root>
+//     );
+//   }
+// );
+
+// export const SelectItem = React.forwardRef(
+//   ({ children, ...props }, forwardedRef) => {
+//     return (
+//       <SelectPrimitive.Item {...props} ref={forwardedRef}>
+//         <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+//         <SelectPrimitive.ItemIndicator>
+//           <CheckIcon />
+//         </SelectPrimitive.ItemIndicator>
+//       </SelectPrimitive.Item>
+//     );
+//   }
+// );
