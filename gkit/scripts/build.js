810d439a7016d79f4411c55f970fdf6798022639
afrokick <devbyru@gmail.com>
2022-08-18 11:35:15 +0300

    release 0.0.75

--- gkit/scripts/build.js
+++ gkit/scripts/build.js
@@ -39,6 +39,7 @@ const isProduction = process.env.NODE_ENV === 'production';
       'classnames',
       'focus-trap-react',
       '@radix-ui/react-dropdown-menu',
+      '@radix-ui/react-select',
       '@radix-ui/react-toggle-group',
       '@radix-ui/react-popover',
       '@radix-ui/react-tooltip',
