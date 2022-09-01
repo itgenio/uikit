588b9cbe865773637be76f29e5189a274eab3be9
Nikita Tsyklinskiy <ntsyklinskiy@gmail.com>
2022-09-01 17:16:55 +0300

    add medium light

--- gkit/src/components/emoji/raisedHand.tsx
+++ gkit/src/components/emoji/raisedHand.tsx
@@ -1,8 +1,15 @@
 import classNames from 'classnames';
 import React from 'react';
 import RaisedHandColorMedium from './assets/raised_hand_color_medium.svg';
+import RaisedHandColorMediumLight from './assets/raised_hand_color_medium_light.svg';
 import { EmojiProps } from './types';
 
 export const RaisedHandColorMediumEmoji = ({ className, ...props }: Partial<EmojiProps> = {}) => {
   return <RaisedHandColorMedium className={classNames('raised-hand-color-medium-emoji', className)} {...props} />;
+};
+
+export const RaisedHandColorMediumLightEmoji = ({ className, ...props }: Partial<EmojiProps> = {}) => {
+  return (
+    <RaisedHandColorMediumLight className={classNames('raised-hand-color-medium-light-emoji', className)} {...props} />
+  );
 };
