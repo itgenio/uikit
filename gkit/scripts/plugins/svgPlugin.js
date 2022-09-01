cad7239185fc5854eb18dc50610921ad29ad8d64
Nikita Kroe <47102190+nkroe@users.noreply.github.com>
2022-09-01 11:42:08 +0500

    refactor/emoji: Рефакторинг эмодзи (#77)
    * refactor/emoji
    
    * fix
    
    * revert
    
    * tsc
    
    * fix regexp
    
    * refactor
    
    * refactor readme

---
+++ gkit/scripts/plugins/svgPlugin.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const path = require('path');
+const svgrTransform = require('@svgr/core').transform;
+
+const escapedPathSep = [...path.sep].map(char => `\\${char}`).join('');
+
+const ICONS_PATH_FILTER = new RegExp([`icons`, `assets`, `\\w+\\.svg$`].join(escapedPathSep));
+const EMOJI_PATH_FILTER = new RegExp([`emoji`, `assets`, `\\w+\\.svg$`].join(escapedPathSep));
+
+const getContents = async ({ svgFilePath, svgrConfig = {}, svgoConfig = {}, svgProps = {} }) => {
+  const svgContent = await fs.promises.readFile(svgFilePath, 'utf8');
+
+  return await svgrTransform(
+    svgContent,
+    {
+      expandProps: 'start',
+      titleProp: true,
+      dimensions: false,
+      plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+      svgoConfig: {
+        plugins: [
+          { name: 'preset-default', params: { overrides: { removeViewBox: false } } },
+          { name: 'removeAttrs', params: { attrs: 'svg:fill' } },
+          'removeXMLNS',
+          'prefixIds',
+        ],
+        ...svgoConfig,
+      },
+      svgProps: {
+        focusable: false,
+        color: 'inherit',
+        'aria-hidden': '{props.title ? undefined : true}',
+        ...svgProps,
+      },
+      ...svgrConfig,
+    },
+    { filePath: svgFilePath }
+  );
+};
+
+module.exports = () => ({
+  name: 'svg',
+  setup(build) {
+    // Icons
+    build.onLoad({ filter: ICONS_PATH_FILTER }, async args => {
+      const contents = await getContents({
+        svgFilePath: args.path,
+        svgProps: { className: '{"gkit-svg-icon" + (props.className ? " " + props.className : "")}' },
+        svgrConfig: { replaceAttrValues: { '#212121': 'currentColor' } },
+      });
+
+      return {
+        contents,
+        loader: 'tsx',
+      };
+    });
+
+    // Emoji
+    build.onLoad({ filter: EMOJI_PATH_FILTER }, async args => {
+      const contents = await getContents({
+        svgFilePath: args.path,
+        svgProps: { className: '{"gkit-emoji" + (props.className ? " " + props.className : "")}' },
+      });
+
+      return {
+        contents,
+        loader: 'tsx',
+      };
+    });
+  },
+});
