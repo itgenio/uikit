665c73ef9702a60a4afd882ac3c8e9672cb3dc8b
AndreyAndreev <888andrei1983888@gmail.com>
2022-09-13 09:42:11 +0000

    todo removed

--- gkit/src/components/icons/wallet.tsx
+++ gkit/src/components/icons/wallet.tsx
@@ -4,6 +4,5 @@ import Wallet from './assets/wallet_24_regular.svg';
 import { SvgIconProps } from './types';
 
 export function WalletIcon({ className, ...props }: Partial<SvgIconProps> = {}) {
-  //TODO: remove
   return <Wallet className={classNames('wallet-icon', className)} {...props} />;
 }
