1fe9e1794d2eeb99348068eecc9ad1e2e9337a17
andrey <belosheykinandrey@gmail.com>
2022-09-28 13:16:43 +0500

    Add filled icons

--- gkit/src/components/icons/arrowDownload.tsx
+++ gkit/src/components/icons/arrowDownload.tsx
@@ -1,8 +1,13 @@
 import classNames from 'classnames';
 import React from 'react';
-import ArrowDownload from './assets/arrow_download.svg';
+import ArrowDownloadFilled from './assets/arrow_download_24_filled.svg';
+import ArrowDownload from './assets/arrow_download_24_regular.svg';
 import { SvgIconProps } from './types';
 
 export function ArrowDownloadIcon({ className, ...props }: Partial<SvgIconProps> = {}) {
   return <ArrowDownload className={classNames('arrow-download-icon', className)} {...props} />;
+}
+
+export function ArrowDownloadFilledIcon({ className, ...props }: Partial<SvgIconProps> = {}) {
+  return <ArrowDownloadFilled className={classNames('arrow-download-icon', className)} {...props} />;
 }
--- gkit/src/components/icons/videoCamera.tsx
+++ gkit/src/components/icons/videoCamera.tsx
@@ -1,8 +1,13 @@
 import classNames from 'classnames';
 import React from 'react';
-import VideoCamera from './assets/video_camera.svg';
+import VideoCameraFilled from './assets/video_24_filled.svg';
+import VideoCamera from './assets/video_24_regular.svg';
 import { SvgIconProps } from './types';
 
 export function VideoCameraIcon({ className, ...props }: Partial<SvgIconProps> = {}) {
   return <VideoCamera className={classNames('arrow-download-icon', className)} {...props} />;
+}
+
+export function VideoCameraFilledIcon({ className, ...props }: Partial<SvgIconProps> = {}) {
+  return <VideoCameraFilled className={classNames('arrow-download-icon', className)} {...props} />;
 }
