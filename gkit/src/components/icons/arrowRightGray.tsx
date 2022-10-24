09185f5e08f0e03d646fe9708337c490363f9026
Vlad <vladkruglenia@yandex.ru>
2022-10-24 14:27:29 +0300

    replaced arrow right icon

--- gkit/src/components/icons/arrowRightGray.tsx
+++
@@ -1,8 +0,0 @@
-import classNames from 'classnames';
-import React from 'react';
-import RightArrow from './/assets/arrow_right_gray.svg';
-import { SvgIconProps } from './types';
-
-export function RightArrowIcon({ className, ...props }: Partial<SvgIconProps> = {}) {
-  return <RightArrow className={classNames('arrow-right-icon', className)} {...props} />;
-}
