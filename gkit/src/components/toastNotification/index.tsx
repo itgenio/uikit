84b22fc70acda6bf156f36d3690fb984f6ef0c9b
Nikita Kroe <nikitak123@mail.ru>
2022-12-05 16:52:53 +0400

    fix onClose animation

--- gkit/src/components/toastNotification/index.tsx
+++ gkit/src/components/toastNotification/index.tsx
@@ -2,8 +2,9 @@ import './style.less';
 
 import * as ToastPrimitive from '@radix-ui/react-toast';
 import classNames from 'classnames';
-import React, { ReactNode } from 'react';
+import React, { Fragment, ReactNode, useRef, useState } from 'react';
 import { ErrorCircleFilledIcon, DismissIcon } from '../icons';
+import { Portal } from '../portal';
 
 type Variants = 'success' | 'warning' | 'error' | 'info';
 
@@ -40,47 +41,94 @@ export const ToastNotification = ({
   durationRoot,
   onClose,
 }: ToastNotificationProps) => {
+  const viewportRef = useRef<HTMLOListElement>(null);
+
   return (
     <ToastPrimitive.Provider {...{ duration, label, swipeDirection, swipeThreshold }}>
-      {/*отображаем в обратном порядке*/}
-      {notifications.reverse().map((notification, index) => {
-        const { id, title, content, variant, ...notificationProps } = notification;
-        const sortIndex = notifications.length - 1 - index;
-
+      {notifications.map((notification, index) => {
         return (
-          <ToastPrimitive.Root
-            key={id}
-            style={{ ['--index' as string]: sortIndex }}
-            id-qa={idQa}
-            className={`gkit-toast-notification gkit-toast-notification-${index}`}
-            hidden={sortIndex >= renderLimit}
-            duration={durationRoot}
-            onOpenChange={open => {
-              if (!open) {
-                onClose(notification);
-              }
-            }}
-            {...notificationProps}
-          >
-            <div className={classNames('toast-inner', className, variant)}>
-              <ErrorCircleFilledIcon className="toast-error-circle-icon" />
-
-              <div className="toast-content-wrapper">
-                <ToastPrimitive.Title className="toast-title">
-                  {title}
-                  <ToastPrimitive.Close className="toast-close">
-                    <DismissIcon className="toast-dismiss-icon" />
-                  </ToastPrimitive.Close>
-                </ToastPrimitive.Title>
-
-                {content}
-              </div>
-            </div>
-          </ToastPrimitive.Root>
+          <Notification
+            key={notification.id}
+            durationRoot={durationRoot}
+            notification={notification}
+            index={index}
+            sortIndex={notifications.length - 1 - index}
+            renderLimit={renderLimit}
+            onClose={onClose}
+            className={className}
+            viewportElement={viewportRef.current}
+            idQa={idQa}
+          />
         );
       })}
 
-      <ToastPrimitive.Viewport className="toast-viewport" hotkey={hotkey} />
+      <ToastPrimitive.Viewport className="toast-viewport" hotkey={hotkey} ref={viewportRef} />
     </ToastPrimitive.Provider>
+  );
+};
+
+const Notification = ({
+  notification,
+  index,
+  sortIndex,
+  renderLimit,
+  durationRoot,
+  idQa,
+  onClose,
+  className,
+  viewportElement,
+}: {
+  notification: NotificationProps;
+  index: number;
+  sortIndex: number;
+  viewportElement: HTMLOListElement | null;
+} & Pick<ToastNotificationProps, 'durationRoot' | 'renderLimit' | 'onClose' | 'className' | 'idQa'>) => {
+  const { id, title, content, variant, ...notificationProps } = notification;
+
+  const [rootRef, setRootRef] = useState<HTMLLIElement>(null);
+
+  const computedStyles = rootRef ? getComputedStyle(rootRef) : undefined;
+  const hidden = sortIndex >= renderLimit;
+
+  return (
+    <Fragment key={id}>
+      <ToastPrimitive.Root
+        style={{ ['--index' as string]: sortIndex }}
+        id-qa={idQa}
+        className={classNames(`gkit-toast-notification gkit-toast-notification-${index}`, { hidden })}
+        duration={durationRoot}
+        onOpenChange={open => {
+          if (!open) {
+            onClose(notification);
+          }
+        }}
+        ref={ref => setRootRef(ref)}
+        {...notificationProps}
+      >
+        <div className={classNames('toast-inner', className, variant)}>
+          <ErrorCircleFilledIcon className="toast-error-circle-icon" />
+
+          <div className="toast-content-wrapper">
+            <ToastPrimitive.Title className="toast-title">
+              {title}
+              <ToastPrimitive.Close className="toast-close">
+                <DismissIcon className="toast-dismiss-icon" />
+              </ToastPrimitive.Close>
+            </ToastPrimitive.Title>
+
+            {content}
+          </div>
+        </div>
+      </ToastPrimitive.Root>
+
+      {viewportElement && (
+        <Portal container={viewportElement}>
+          <div
+            className={classNames('gkit-toast-notification-background', { hidden })}
+            style={{ width: computedStyles?.width, height: computedStyles?.height }}
+          />
+        </Portal>
+      )}
+    </Fragment>
   );
 };
