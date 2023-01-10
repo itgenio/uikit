63d52e394e9a6558392cacc8764a2bfb17dc1279
Nikita Kroe <47102190+nkroe@users.noreply.github.com>
2023-01-10 16:45:06 +0400

    optimization/minify (#125)
    * optimization/minify
    
    * fix modal body styles
    
    * build target
    
    * update version
    
    * update version

--- gkit/scripts/build.js
+++ gkit/scripts/build.js
@@ -29,9 +29,11 @@ const isProduction = process.env.NODE_ENV === 'production';
           },
         },
     format: 'esm',
+    target: 'es6',
     keepNames: true,
     outdir: path.resolve(__dirname, '..'),
     entryPoints,
+    minify: true,
     bundle: true,
     write: false, // check writePlugin
     external: [
