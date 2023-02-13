b4533c6910d8caa491b96ef29c02cd0089d86a1c
vladislav-kruglenia <64707382+vladislav-kruglenia@users.noreply.github.com>
2023-02-13 13:59:55 +0300

    Cypress/GENA-12031 - added id-qa in multiselect count (#141)
    * added id-qa in multiselect count
    
    * added id-qa in multiselect count - fix
    
    ---------
    
    Co-authored-by: Vlad <vladkruglenia@yandex.ru>

--- gkit/src/components/multiSelect/index.tsx
+++ gkit/src/components/multiSelect/index.tsx
@@ -108,6 +108,7 @@ export function MultiSelect<T extends MultiSelectOption>({
       <Fragment>
         {hasValue && !disabled && (
           <span
+            id-qa={classNames({ [`${idQa}-count`]: idQa })}
             className={classNames('multi-select-count', size)}
             onClick={e => {
               e.stopPropagation();
