cfc974d287af3bcdb11ad97dfe9fda46a3d0e435
Nikita Kroe <nikitak123@mail.ru>
2023-08-21 15:29:17 +0500

    add light-color, fix green 200

--- app/src/pages/uikit/components/colors/index.tsx
+++ app/src/pages/uikit/components/colors/index.tsx
@@ -19,7 +19,7 @@ export function Colors() {
               {name}
             </p>
             <div className="modes">
-              <div className="shadow-md">
+              <div className="shadow-md light-mode">
                 {nums.map(n => (
                   <div
                     key={`${name}${n}`}
