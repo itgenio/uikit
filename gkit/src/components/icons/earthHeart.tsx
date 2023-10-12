102922fe441590ba695b28a5febb8a32ea6d9c74
Vlad <aniskovec.vlad@mail.ru>
2023-10-12 17:26:08 +0300

    fix icon name

--- gkit/src/components/icons/earthHeart.tsx
+++ gkit/src/components/icons/earthHeart.tsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import React from 'react';
 import EarthHeartFilled from './assets/earth_heart_24_filled.svg';
-import EarthHeart from './assets/arrow_clockwise_24_regular.svg';
+import EarthHeart from './assets/earth_heart_24_regular.svg';
 import { SvgIconProps } from './types';
 
 export function EarthHeartIcon({ className, ...props }: Partial<SvgIconProps> = {}) {
