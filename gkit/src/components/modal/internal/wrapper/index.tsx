a89a0dfb9dd8beb0793a97dd8f8c9c46e15e7dea
Nikita Kroe <nikitak123@mail.ru>
2024-01-11 02:36:11 +0200

    remove note

--- gkit/src/components/modal/internal/wrapper/index.tsx
+++ gkit/src/components/modal/internal/wrapper/index.tsx
@@ -10,7 +10,6 @@ type Props = Pick<ModalProps, 'children'>;
 
 export const ModalWrapperInternal = React.memo(({ children }: Props) => {
   return (
-    // При динамической загрузке у FocusTrap'a нет tabbable elements, поэтому нужно передать селектор в fallbackFocus
     <FocusLock className={`${MODAL_WRAPPER_CN}-focus-lock`}>
       <div className={MODAL_WRAPPER_CN}>{children}</div>
     </FocusLock>
