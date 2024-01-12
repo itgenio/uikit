40ea99538cecccb9a575429dedbb95419bfb904b
Nikita TS <ntsyklinskiy@gmail.com>
2024-01-12 13:46:08 +0300

    fix

--- gkit/src/components/emoji/partyingFace.tsx
+++ gkit/src/components/emoji/partyingFace.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 import PatyingFaceColor from './assets/partying_face_color.svg';
 import { EmojiProps } from './types';
 
-export const PatyingFace = ({ className, ...props }: Partial<EmojiProps> = {}) => {
+export const PatyingFaceEmoji = ({ className, ...props }: Partial<EmojiProps> = {}) => {
   return <PatyingFaceColor className={classNames('partying-face-color-emoji', className)} {...props} />;
 };
--- gkit/src/components/emoji/redQuestionMark.tsx
+++ gkit/src/components/emoji/redQuestionMark.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 import RedQuestionMarkColor from './assets/red_question_mark_color.svg';
 import { EmojiProps } from './types';
 
-export const RedQuestionMark = ({ className, ...props }: Partial<EmojiProps> = {}) => {
+export const RedQuestionMarkEmoji = ({ className, ...props }: Partial<EmojiProps> = {}) => {
   return <RedQuestionMarkColor className={classNames('red-question-mark-color-emoji', className)} {...props} />;
 };
