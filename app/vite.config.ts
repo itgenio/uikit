e3ecbbc281e5b0e8d6343cfa9fef3ca7f175b0d9
Nikita Kroe <nikitak123@mail.ru>
2024-04-18 00:25:57 +0300

    fix build

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,22 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import dynamicImport from 'vite-plugin-dynamic-import';
+// import dynamicImport from './dynamicImport';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dynamicImport()],
+  plugins: [
+    react(),
+    dynamicImport({
+      onFiles(files) {
+        return files.filter(file => {
+          return (
+            !file.includes('node_modules/@itgenio/icons') ||
+            /node_modules\/@itgenio\/icons\/\w+.(\.d\.ts|\.js)/.test(file)
+          );
+        });
+      },
+    }),
+  ],
   preview: { port: 3005 },
 });
