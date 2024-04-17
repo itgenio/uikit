e0cec9a438960aadb1eaab9428e1e977e419762c
Nikita Kroe <nikitak123@mail.ru>
2024-04-18 00:37:56 +0300

    remove comment

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import dynamicImport from 'vite-plugin-dynamic-import';
-// import dynamicImport from './dynamicImport';
 
 // https://vitejs.dev/config/
 export default defineConfig({
