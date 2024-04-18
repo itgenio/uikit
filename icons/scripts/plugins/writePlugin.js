7227d6c355040eab4e4293f03eaafcca17631406
Nikita Kroe <nikitak123@mail.ru>
2024-04-18 19:14:53 +0300

    rename icons

--- icons/scripts/plugins/writePlugin.js
+++ icons/scripts/plugins/writePlugin.js
@@ -5,87 +5,92 @@ const { groupByPropertyToDict, chunkArray } = require('@itgenio/utils');
 const JS_EXT = '.js';
 const TS_DEC_EXT = '.d.ts';
 const ICONS_LIST_FILENAME = 'iconsList';
+const FLUENT_ICON_PREFIX = 'ic_fluent_';
 
-module.exports = () => ({
-  name: 'write',
-  setup(build) {
-    const outDir = path.resolve(process.cwd());
+module.exports = {
+  FLUENT_ICON_PREFIX,
+  writePlugin: () => ({
+    name: 'write',
+    setup(build) {
+      const outDir = path.resolve(process.cwd());
 
-    build.onEnd(async result => {
-      const files = result.outputFiles;
+      build.onEnd(async result => {
+        const files = result.outputFiles;
 
-      const filesDict = groupByPropertyToDict(files, file => file.path);
+        const filesDict = groupByPropertyToDict(files, file => file.path);
 
-      const iconsList = [];
+        const iconsList = [];
 
-      const filesChunks = chunkArray(files, 100);
+        const filesChunks = chunkArray(files, 100);
 
-      for (const chunk of filesChunks) {
-        await Promise.allSettled(
-          chunk.map(async file => {
-            const filePath = file.path;
-            let fileContent = file.text;
+        for (const chunk of filesChunks) {
+          await Promise.allSettled(
+            chunk.map(async file => {
+              const filePath = file.path;
+              let fileContent = file.text;
 
-            const fileDir = path.dirname(filePath);
-            let fileName = path.basename(filePath);
-            const fileNameWithoutExt = fileName.replace(JS_EXT, '');
+              const fileDir = path.dirname(filePath);
+              let fileName = path.basename(filePath).replace(FLUENT_ICON_PREFIX, '');
 
-            const fileNameParts = fileName.replace(JS_EXT, '').split('_');
+              const fileNameWithoutExt = fileName.replace(JS_EXT, '');
 
-            const isRegular = fileNameParts.at(-1) === 'regular';
+              const fileNameParts = fileName.replace(JS_EXT, '').split('_');
 
-            fileNameParts.splice(fileNameParts.length - 2, isRegular ? 2 : 1);
-            fileNameParts.push('icon');
+              const isRegular = fileNameParts.at(-1) === 'regular';
 
-            const promises = [];
+              fileNameParts.splice(fileNameParts.length - 2, isRegular ? 2 : 1);
+              fileNameParts.push('icon');
 
-            if (filePath.endsWith(JS_EXT)) {
-              const cssFileRelativePath = `./${fileNameWithoutExt}.css`;
-              const cssFile = filesDict[path.resolve(fileDir, cssFileRelativePath)];
+              const promises = [];
 
-              if (cssFile) {
-                fileContent = `import '${cssFileRelativePath}';\n${fileContent}`;
+              if (filePath.endsWith(JS_EXT)) {
+                const cssFileRelativePath = `./${fileNameWithoutExt}.css`;
+                const cssFile = filesDict[path.resolve(fileDir, cssFileRelativePath)];
+
+                if (cssFile) {
+                  fileContent = `import '${cssFileRelativePath}';\n${fileContent}`;
+                }
+
+                if (fileNameWithoutExt !== 'index') {
+                  const componentName = fileNameParts.map(part => part[0].toUpperCase() + part.slice(1)).join('');
+                  const fileNameWithoutExt = componentName[0].toLowerCase() + componentName.slice(1);
+
+                  iconsList.push([fileNameWithoutExt, componentName]);
+
+                  fileName = `${fileNameWithoutExt}${JS_EXT}`;
+
+                  promises.push(
+                    fs.promises.writeFile(
+                      path.resolve(outDir, `${fileNameWithoutExt}${TS_DEC_EXT}`),
+                      `/// <reference types="react" />
+  import { SvgIconProps } from './types';
+  export declare function ${componentName}({ className, ...props }?: Partial<SvgIconProps>): JSX.Element;
+                `
+                    )
+                  );
+                }
               }
 
-              if (fileNameWithoutExt !== 'index') {
-                const componentName = fileNameParts.map(part => part[0].toUpperCase() + part.slice(1)).join('');
-                const fileNameWithoutExt = componentName[0].toLowerCase() + componentName.slice(1);
+              promises.push(fs.promises.writeFile(path.resolve(outDir, fileName), fileContent));
 
-                iconsList.push([fileNameWithoutExt, componentName]);
+              await Promise.allSettled(promises);
+            })
+          );
+        }
 
-                fileName = `${fileNameWithoutExt}${JS_EXT}`;
-
-                promises.push(
-                  fs.promises.writeFile(
-                    path.resolve(outDir, `${fileNameWithoutExt}${TS_DEC_EXT}`),
-                    `/// <reference types="react" />
-import { SvgIconProps } from './types';
-export declare function ${componentName}({ className, ...props }?: Partial<SvgIconProps>): JSX.Element;
-              `
-                  )
-                );
-              }
-            }
-
-            promises.push(fs.promises.writeFile(path.resolve(outDir, fileName), fileContent));
-
-            await Promise.allSettled(promises);
-          })
-        );
-      }
-
-      await Promise.allSettled([
-        fs.promises.writeFile(
-          path.resolve(outDir, `${ICONS_LIST_FILENAME}${JS_EXT}`),
-          `var iconsList = ${JSON.stringify(iconsList)};
-export { iconsList };
-`
-        ),
-        fs.promises.writeFile(
-          path.resolve(outDir, `${ICONS_LIST_FILENAME}${TS_DEC_EXT}`),
-          `export declare var iconsList: [string, string][];`
-        ),
-      ]);
-    });
-  },
-});
+        await Promise.allSettled([
+          fs.promises.writeFile(
+            path.resolve(outDir, `${ICONS_LIST_FILENAME}${JS_EXT}`),
+            `var iconsList = ${JSON.stringify(iconsList)};
+  export { iconsList };
+  `
+          ),
+          fs.promises.writeFile(
+            path.resolve(outDir, `${ICONS_LIST_FILENAME}${TS_DEC_EXT}`),
+            `export declare var iconsList: [string, string][];`
+          ),
+        ]);
+      });
+    },
+  }),
+};
--- icons/src/fluent/README.md
+++ icons/src/fluent/README.md
@@ -6,12 +6,12 @@
 
 Оригинал нужной иконки можно найти здесь: https://github.com/microsoft/fluentui-system-icons/find/main
 
-Иконки добавляются в исходном виде в папку SVG, нужно убрать только префикс "ic*fluent*"
+Иконки добавляются в исходном виде в папку SVG
 
-Обязательное соблюдение формата названия иконки: name_size_type.svg
+Обязательное соблюдение формата названия иконки как в оригинале: name_size_type.svg
 
 Исключения, эти иконки удалены т.к дублируются названия, но контент не отличается:
-reorder_20_filled.svg
-reorder_20_filled.svg
-text_box_settings_24_regular.svg
-text_box_settings_24_filled.svg
+ic_fluent_reorder_20_regular.svg
+ic_fluent_reorder_20_filled.svg
+ic_fluent_text_box_settings_24_regular.svg
+ic_fluent_text_box_settings_24_filled.svg
