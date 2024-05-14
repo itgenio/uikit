4d4913c576a4f685fd4d07a4c0b4c8b118c7f295
AndreevAndrey <51719046+AndreevAndrey@users.noreply.github.com>
2024-05-14 13:52:22 +0000

    fix loader (#200)
    * fix loader
    
    * remove white
    
    * minor edit

--- gkit/src/components/loader/index.tsx
+++ gkit/src/components/loader/index.tsx
@@ -3,26 +3,29 @@ import './style.less';
 import classNames from 'classnames';
 import React from 'react';
 
-type Sizes = 'small' | 'normal' | 'large';
-type Types = 'primary';
+export type Sizes = 'small' | 'normal' | 'large';
+export type Types = 'primary';
 
 export const LOADER_CN = 'gkit-loader';
 
 type Props = {
   className?: string;
-  size: Sizes;
+  size?: Sizes;
   type?: Types;
   text?: React.ReactNode;
   isLoading?: boolean;
+  idQa?: string;
 };
 
-export const Loader = React.memo(({ text, className, size = 'normal', type = 'primary', isLoading = true }: Props) => {
-  return isLoading ? (
-    <span className={classNames(LOADER_CN, className, type, size)}>
-      <span className="gkit-loader-circle" />
-      {!text ? null : <span className="gkit-loader-text">{text}</span>}
-    </span>
-  ) : null;
-});
+export const Loader = React.memo(
+  ({ text, className, size = 'normal', type = 'primary', isLoading = true, idQa }: Props) => {
+    return isLoading ? (
+      <span className={classNames(LOADER_CN, className, type, size)} id-qa={idQa}>
+        <span className={`${LOADER_CN}-circle`} />
+        {!text ? null : <span className={`${LOADER_CN}-text`}>{text}</span>}
+      </span>
+    ) : null;
+  }
+);
 
 Loader.displayName = 'Loader';
