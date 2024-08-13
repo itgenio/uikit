5d6552cc1c178c65a8978464608a5d0b3f9cf4af
AndreyAndreev <888andrei1983888@gmail.com>
2024-08-13 06:29:07 +0000

    edits by comments

--- gkit/src/components/slider/index.tsx
+++ gkit/src/components/slider/index.tsx
@@ -7,37 +7,19 @@ import React from 'react';
 export type SliderProps = {
   idQa?: string;
   idQaThumb?: string;
-  className?: string;
-  value?: number[];
-  defaultValue?: number[];
-  onValueChange?: (val: number[]) => void;
-  min?: number;
-  max?: number;
-  step?: number;
-  disabled?: boolean;
-};
+} & SliderPrimitive.SliderProps;
 
-export const Slider = React.memo(
-  ({ idQa, idQaThumb, className, value, onValueChange, defaultValue, min, max, step, disabled }: SliderProps) => {
-    return (
-      <SliderPrimitive.Root
-        id-qa={idQa}
-        className={classNames('gkit-slider', className)}
-        defaultValue={defaultValue}
-        min={min}
-        max={max}
-        step={step}
-        onValueChange={onValueChange}
-        value={value}
-        disabled={disabled}
-      >
-        <SliderPrimitive.Track className="gkit-slider-track">
-          <SliderPrimitive.Range className="gkit-slider-range" />
-        </SliderPrimitive.Track>
-        <SliderPrimitive.Thumb className="gkit-slider-thumb" id-qa={idQaThumb} />
-      </SliderPrimitive.Root>
-    );
-  }
-);
+const SLIDER_CN = 'gkit-slider';
+
+export const Slider = React.memo(({ idQa, idQaThumb, className, ...props }: SliderProps) => {
+  return (
+    <SliderPrimitive.Root {...props} id-qa={idQa} className={classNames(SLIDER_CN, className)}>
+      <SliderPrimitive.Track className={`${SLIDER_CN}-track`}>
+        <SliderPrimitive.Range className={`${SLIDER_CN}-range`} />
+      </SliderPrimitive.Track>
+      <SliderPrimitive.Thumb className={`${SLIDER_CN}-thumb`} id-qa={idQaThumb} />
+    </SliderPrimitive.Root>
+  );
+});
 
 Slider.displayName = 'Slider';
