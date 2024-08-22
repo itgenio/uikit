b37239c973009caf94fbb9173d3f13b0bdb36a03
AndreyAndreev <888andrei1983888@gmail.com>
2024-08-22 07:40:48 +0000

    revert v react-focus-lock to old, removed modal-wrapper

--- gkit/src/components/modal/internal/wrapper/index.tsx
+++ gkit/src/components/modal/internal/wrapper/index.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import FocusLock from 'react-focus-lock';
 import { ModalProps } from '../..';
 
-const MODAL_WRAPPER_CN = 'modal-wrapper';
-
 type Props = Pick<ModalProps, 'children'>;
 
 export const ModalWrapperInternal = React.memo(({ children }: Props) => {
   return (
     <FocusLock
-      className={`${MODAL_WRAPPER_CN}-focus-lock`}
+      className="modal-wrapper-focus-lock"
       whiteList={node => !document.getElementById('dynamic-react')?.contains(node)}
     >
-      <div className={MODAL_WRAPPER_CN}>{children}</div>
+      {children}
     </FocusLock>
   );
 });
