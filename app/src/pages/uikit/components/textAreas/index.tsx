5991957ea6efccd0c00ddc919fa01b46fc2e0bec
AndreevAndrey <51719046+AndreevAndrey@users.noreply.github.com>
2024-12-20 13:58:05 +0000

    fix/GENA-17059-textArea-withAutoHeight-clear (#223)
    * fix/GENA-17059-textArea-withAutoHeight-clear
    
    * added isControllableState
    
    * fix, added controllable state
    
    * fix, added controllable state
    
    * fix

--- app/src/pages/uikit/components/textAreas/index.tsx
+++ app/src/pages/uikit/components/textAreas/index.tsx
@@ -1,23 +1,62 @@
 import './style.less';
-import React, { Fragment } from 'react';
+import React, { Fragment, useCallback, useState } from 'react';
+import { Button } from '@itgenio/gkit/button';
 import { TextArea, TextAreaProps } from '@itgenio/gkit/textArea';
+
+type Settings = { controllable?: boolean; clearStateButton?: boolean };
 
 export function TextAreas() {
   const sizes = ['small', 'large'] as const;
+  const [value, setValue] = useState('text\ntext\ntext');
 
-  const renderState = (state: string, props: TextAreaProps, index: number) => {
+  const clearState = useCallback(() => setValue(''), []);
+
+  const renderState = ({
+    state,
+    props,
+    settings,
+    index,
+  }: {
+    state: string;
+    props: TextAreaProps;
+    settings: Settings;
+    index: number;
+  }) => {
     return (
       <Fragment key={index}>
         <div>{state}</div>
         {sizes.map(size => {
           const p = { ...props, size };
-          return <TextArea {...p} placeholder="Placeholder" key={size} label="Label" helperText="Desc" />;
+          const { controllable, clearStateButton } = settings;
+
+          return (
+            <div className="text-area-wrapper" key={`${index}${size}`}>
+              <TextArea
+                {...p}
+                value={controllable ? value : p.value}
+                onChange={p.onChange ? e => setValue(e.target.value) : undefined}
+                placeholder="Placeholder"
+                key={size}
+                label="Label"
+                helperText="Desc"
+              />
+              {clearStateButton && size === 'small' && (
+                <Button size="small" onClick={clearState}>
+                  Clear state
+                </Button>
+              )}
+            </div>
+          );
         })}
       </Fragment>
     );
   };
 
-  const states: { state: string; props?: TextAreaProps }[] = [
+  const states: {
+    state: string;
+    props?: TextAreaProps;
+    settings?: Settings;
+  }[] = [
     { state: 'Normal' },
     { state: 'Hover', props: { hover: true } },
     { state: 'Filled', props: { filled: true } },
@@ -25,12 +64,22 @@ export function TextAreas() {
     { state: 'Error', props: { error: true } },
     { state: 'Disabled', props: { disabled: true, value: 'text', resize: 'none' } },
     { state: 'AutoFocus', props: { autoFocus: true, value: 'text', onChange: () => {} } },
-    { state: 'With autoHeight', props: { withAutoHeight: true } },
+    {
+      state: 'With autoHeight (uncontrollable)',
+      props: { withAutoHeight: true, onChange: () => {} },
+    },
+    {
+      state: 'With autoHeight (controllable)',
+      props: { withAutoHeight: true, onChange: () => {} },
+      settings: { controllable: true, clearStateButton: true },
+    },
   ];
 
   return (
     <div className="textAreas">
-      <div className="grid">{states.map(({ state, props = {} }, index) => renderState(state, props, index))}</div>
+      <div className="grid">
+        {states.map(({ state, props = {}, settings = {} }, index) => renderState({ state, props, settings, index }))}
+      </div>
     </div>
   );
 }
