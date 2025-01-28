cdf3c2d91a525b5e8151266d936f93c08c0f5844
Siarhey1607 <izaspenec@gmail.com>
2025-01-28 17:54:34 +0100

    cr/GENA-19246

--- gkit/src/components/loader/index.tsx
+++ gkit/src/components/loader/index.tsx
@@ -1,9 +1,9 @@
 import './style.less';
 
 import classNames from 'classnames';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-export type Sizes = 'small' | 'normal' | 'large';
+export type Sizes = 'small' | 'normal' | 'large' | number;
 export type Types = 'primary';
 
 export const LOADER_CN = 'gkit-loader';
@@ -19,8 +19,21 @@ type Props = {
 
 export const Loader = React.memo(
   ({ text, className, size = 'normal', type = 'primary', isLoading = true, idQa }: Props) => {
+    const ref = useRef<HTMLSpanElement>(null);
+
+    useEffect(() => {
+      const containerElement = ref.current;
+      if (!containerElement || typeof size !== 'number') return;
+
+      containerElement.style.setProperty('--gkit-loader-size', `${size}px`);
+    }, [size]);
+
     return isLoading ? (
-      <span className={classNames(LOADER_CN, className, type, size)} id-qa={idQa}>
+      <span
+        ref={ref}
+        className={classNames(LOADER_CN, className, type, { [size]: typeof size !== 'number' })}
+        id-qa={idQa}
+      >
         <span className={`${LOADER_CN}-circle`} />
         {!text ? null : <span className={`${LOADER_CN}-text`}>{text}</span>}
       </span>
